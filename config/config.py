2dfca42c4e24b50a73f11971dda5f153a3f235dd
gagliardi-matteo <72274102+gagliardi-matteo@users.noreply.github.com>
2025-10-20 15:08:56 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-GEMINI_API_KEY = "AIzaSyDFcDLEGQxp301ph5RM-GrRIUe-CrTqRYE"
+GEMINI_API_KEY = "tua-chiave"
 MIC_DEVICE_INDEX = 1          # numero del microfono, se serve
 CAMERA_INDEX = 0              # indice della webcam
 LANGUAGE = "it-IT"  # lingua per STT/TTS
